649fd1f458b187ad1af34d4c88139751b74c6ed6
Hesam-Javadiun <hesam.javadiun@gmail.com>
2022-01-26 15:33:10 +0330

    updated

--- cards/cards.js
+++ cards/cards.js
@@ -1,3 +1,18 @@
 export default function fn(cards) {
-  // return false or [sequence order]
+// function fn(cards) {
+    // let i;
+    
+    // for(i = 0; i < cards.length; i ++){
+    //   if(cards[i] === "1") {
+    //     cards[i] = ".";
+    //     console.log(cards[i]);
+    //     (cards[i - 1] !== ".") && (cards[i - 1] ==="0" ? (cards[i - 1] = "1") : (cards[i - 1] = "0") );
+    //     (cards[i + 1] !== "." ) && (cards[i + 1] ==="0" ? (cards[i - 1] = "1") : (cards[i - 1] = "0") );
+    //   }
+    // }
+    // cards[0] = "a"
+    // console.log(cards)
+  
+  
 }
+
