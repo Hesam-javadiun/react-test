b089b85cd8635d637463eb157f6cc39627952793
Hesam-Javadiun <hesam.javadiun@gmail.com>
2022-01-26 17:40:03 +0330

    last update

--- cards/cards.js
+++ cards/cards.js
@@ -1,18 +1,64 @@
 export default function fn(cards) {
 // function fn(cards) {
-    // let i;
-    
-    // for(i = 0; i < cards.length; i ++){
-    //   if(cards[i] === "1") {
-    //     cards[i] = ".";
-    //     console.log(cards[i]);
-    //     (cards[i - 1] !== ".") && (cards[i - 1] ==="0" ? (cards[i - 1] = "1") : (cards[i - 1] = "0") );
-    //     (cards[i + 1] !== "." ) && (cards[i + 1] ==="0" ? (cards[i - 1] = "1") : (cards[i - 1] = "0") );
-    //   }
-    // }
-    // cards[0] = "a"
-    // console.log(cards)
-  
-  
+  const str = cards
+    .split('')
+    .map(card => '.')
+    .join('');
+
+  const loop = cards => {
+      const masure = (number) =>{
+          if(number >= 0 ){
+              return number
+          }
+          else {
+              return 0
+          }
+      }
+    let i;
+    let perv;
+    let next;
+    for (i = 0; i < cards.length; i++) {
+        if (cards[i] === '1') {
+            perv = cards.slice(0, masure(i - 1)) ? cards.slice(0, masure(i - 1)) : '';
+            if (perv !== '' && perv[perv.length - 1] === '1') {
+                
+                perv = perv.replace( /1$/, '0');
+            } else if (perv !== '' && perv[perv.length - 1] === '1') {
+                perv = perv.replace(/0$/ ,'1' );
+            }
+
+            next = cards.slice( i + 1 , cards.length - 1 ) ? cards.slice(i+ 1, cards.length - 1 ) : '';
+            if (next !== '' && next[0] === '1') {
+                next = next.replace(/1$/, '0' );
+
+            } else if (next !== '' && next[0] === '0') {
+            next = next.replace(/0$/, '1' );
+            }
+        const newCards = perv + '.' + next;
+        console.log("newCards", newCards)
+        const index = i;
+        return { index, newCards };
+    }
+    const newCards = null
+    const index = null
+    return { index, newCards };
+    }
+  };
+  let bol = true;
+  let newCards = cards
+  let index;
+  let resultedString;
+  while(bol) {
+         const result  = loop(cards) 
+         newCards = result.newCards; // in code naghes hastesh va kamel nashode :(
+         index = result.index;
+         
+      if (newCards === null) {
+          bol = false;
+      }
+  }
+  return newCards
 }
+
+
 
